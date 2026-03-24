5f1d1320505f5247f287ce39abcaf19916622d6d
nayon-kumar <nayon.cse55@gmail.com>
2026-03-24 23:54:18 +0600

    Toggle btn added

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Players = () => {
+  const [available, setAvilable] = useState("available");
   return (
-    <div>
-      <p>Players</p>
+    <div className="container mx-auto mt-15">
+      <div className="flex items-center justify-between">
+        <div>
+          <h4 className="font-bold text-2xl">Available Players</h4>
+        </div>
+        <div className="flex">
+          <button
+            onClick={() => setAvilable("available")}
+            className={`btn rounded-r-none ${available === "available" ? "btn-primary" : ""}`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setAvilable("selected")}
+            className={`btn rounded-l-none ${available === "selected" ? "btn-primary" : ""}`}
+          >
+            Selected (0)
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

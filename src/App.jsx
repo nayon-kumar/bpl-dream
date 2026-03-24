a4171959b66242a522546c9603f66f5e40a58044
nayon-kumar <nayon.cse55@gmail.com>
2026-03-24 23:05:35 +0600

    Banner section desinged and Player section design started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
+import Players from "./components/Players/Players";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Players />
     </>
   );
 }

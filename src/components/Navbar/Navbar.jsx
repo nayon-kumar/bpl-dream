24f9ac4ebcab257666acea13e32ff6380ab5edfd
nayon-kumar <nayon.cse55@gmail.com>
2026-03-24 21:56:29 +0600

    Navbar designed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/BPL/logo.png";
 import { Link } from "react-router-dom";
 import { HiCurrencyDollar } from "react-icons/hi";
+import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <div className="flex justify-between bg-base-100 shadow-sm px-3 py-2">
-        <div className="flex items-center sm:hidden">
-          <IoClose size={30} />
+    <div className="relative">
+      <div className="flex justify-baseline gap-2 sm:justify-between bg-base-100 shadow-sm px-3 py-2">
+        <div
+          className="flex items-center sm:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <IoClose size={25} /> : <IoMenu size={25} />}
         </div>
         <div>
-          <img className="h-7 sm:h-12" src={logo} alt="Logo" />
+          <img className="cursor-pointer" src={logo} alt="Logo" />
         </div>
         <div className="hidden sm:flex">
           <div className="flex items-center">
@@ -45,6 +50,44 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+        {isOpen ? (
+          <>
+            <div className="absolute top-18 left-0">
+              <div className="flex flex-col">
+                <Link
+                  to="/"
+                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/"
+                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                >
+                  Fixture
+                </Link>
+                <Link
+                  to="/"
+                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                >
+                  Teams
+                </Link>
+                <Link
+                  to="/"
+                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                >
+                  Schedules
+                </Link>
+                <button className="btn">
+                  0 Coin{" "}
+                  <HiCurrencyDollar className="text-[#E88102]" size={25} />
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );

2cde18ef2ec3164a86e29739efff8cd2aeae6d80
nayon-kumar <nayon.cse55@gmail.com>
2026-03-25 20:13:41 +0600

    Data added from API

--- src/components/Players/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/Players/AvailablePlayers/AvailablePlayers.jsx
@@ -3,7 +3,7 @@ import Card from "../../../ui/Card/Card";
 
 const AvailablePlayers = ({ data }) => {
   return (
-    <div className="grid grid-cols-3 gap-5 mt-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-10">
       {data.map((player, index) => (
         <Card key={index} player={player} />
       ))}
--- src/ui/Card/Card.jsx
+++ src/ui/Card/Card.jsx
@@ -11,17 +11,17 @@ const Card = ({ player }) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            <FaUser /> Card Title
+            <FaUser /> {player.name}
           </h2>
           <div>
             <div className="flex items-center justify-between">
               <div className="flex gap-2 items-center">
                 <CiFlag1 size={25} />
-                <p className="text-[#131313]/50">India</p>
+                <p className="text-[#131313]/50">{player.country}</p>
               </div>
               <div>
                 <p className="border-2 px-2 py-1 border-gray-200 bg-gray-100 rounded-sm">
-                  All-Rounder
+                  {player.player_type}
                 </p>
               </div>
             </div>
@@ -29,13 +29,13 @@ const Card = ({ player }) => {
             <div>
               <p className="font-bold mb-4">Rating</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">Left-Hand-Bat</p>
-                <p className="text-right">Left-Hand-Bowl</p>
+                <p className="font-bold">{player.batting_style}</p>
+                <p className="text-right">{player.bowling_style}</p>
               </div>
             </div>
           </div>
           <div className="flex items-center justify-between">
-            <p className="font-bold">Price: $1500000</p>
+            <p className="font-bold">{player.price}</p>
             <button className="btn hover:btn-primary">Choose Player</button>
           </div>
         </div>

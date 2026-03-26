15b50b66b06e91c20ba7eba60313e722f770508e
nayon-kumar <nayon.cse55@gmail.com>
2026-03-26 22:15:58 +0600

    No player selected section added

--- src/components/Players/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/Players/SelectedPlayers/SelectedPlayers.jsx
@@ -19,34 +19,43 @@ const SelectedPlayers = ({
 
   return (
     <div className="mt-10 flex flex-col gap-4">
-      {selectedPlayer.map((player, index) => {
-        return (
-          <div
-            key={index}
-            className="flex items-center justify-between border-2 border-gray-300 p-5 rounded-xl"
-          >
-            <div className="flex gap-4 items-center">
-              <div>
-                <img
-                  className="h-20 w-20 rounded-xl"
-                  src={player.profile_image}
-                  alt={player.name}
-                />
-              </div>
-              <div>
-                <h3 className="font-bold">{player.name}</h3>
-                <p className="text-[#131313]/60">{player.player_type}</p>
-              </div>
-            </div>
+      {selectedPlayer.length === 0 ? (
+        <div className="text-center">
+          <h3 className="text-xl text-gray-500">No player selected yet</h3>
+          <p className="text-gray-500 mt-2">
+            Go to available tab to select player
+          </p>
+        </div>
+      ) : (
+        selectedPlayer.map((player, index) => {
+          return (
             <div
-              onClick={() => deleteSelectedPlayer(player)}
-              className="cursor-pointer"
+              key={index}
+              className="flex items-center justify-between border-2 border-gray-300 p-5 rounded-xl"
             >
-              <RiDeleteBin6Line size={25} className="text-red-600" />
+              <div className="flex gap-4 items-center">
+                <div>
+                  <img
+                    className="h-20 w-20 rounded-xl"
+                    src={player.profile_image}
+                    alt={player.name}
+                  />
+                </div>
+                <div>
+                  <h3 className="font-bold">{player.name}</h3>
+                  <p className="text-[#131313]/60">{player.player_type}</p>
+                </div>
+              </div>
+              <div
+                onClick={() => deleteSelectedPlayer(player)}
+                className="cursor-pointer"
+              >
+                <RiDeleteBin6Line size={25} className="text-red-600" />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };

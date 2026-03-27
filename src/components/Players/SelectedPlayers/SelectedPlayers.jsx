590a6d728afe5afe37b9e852005b6e0e8737635d
nayon-kumar <nayon.cse55@gmail.com>
2026-03-27 12:53:31 +0600

    Selected card separated

--- src/components/Players/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/Players/SelectedPlayers/SelectedPlayers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { toast } from "react-toastify";
+import SelectedCard from "../../../ui/Card/SelectedCard";
 
 const SelectedPlayers = ({
   selectedPlayer,
@@ -8,15 +7,6 @@ const SelectedPlayers = ({
   price,
   setPrice,
 }) => {
-  const deleteSelectedPlayer = (dPlayer) => {
-    toast.success(`${dPlayer.name} deleted!`);
-    const newPrice = price + parseInt(dPlayer.price);
-    setPrice(newPrice);
-    setSelectedPlayer(
-      selectedPlayer.filter((sPlayer) => sPlayer.name !== dPlayer.name),
-    );
-  };
-
   return (
     <div className="mt-10 flex flex-col gap-4">
       {selectedPlayer.length === 0 ? (
@@ -27,34 +17,16 @@ const SelectedPlayers = ({
           </p>
         </div>
       ) : (
-        selectedPlayer.map((player, index) => {
-          return (
-            <div
-              key={index}
-              className="flex items-center justify-between border-2 border-gray-300 p-5 rounded-xl"
-            >
-              <div className="flex gap-4 items-center">
-                <div>
-                  <img
-                    className="h-20 w-20 rounded-xl"
-                    src={player.profile_image}
-                    alt={player.name}
-                  />
-                </div>
-                <div>
-                  <h3 className="font-bold">{player.name}</h3>
-                  <p className="text-[#131313]/60">{player.player_type}</p>
-                </div>
-              </div>
-              <div
-                onClick={() => deleteSelectedPlayer(player)}
-                className="cursor-pointer"
-              >
-                <RiDeleteBin6Line size={25} className="text-red-600" />
-              </div>
-            </div>
-          );
-        })
+        selectedPlayer.map((player, index) => (
+          <SelectedCard
+            key={index}
+            player={player}
+            selectedPlayer={selectedPlayer}
+            setSelectedPlayer={setSelectedPlayer}
+            price={price}
+            setPrice={setPrice}
+          />
+        ))
       )}
     </div>
   );
---
+++ src/ui/Card/SelectedCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { toast } from "react-toastify";
+
+const SelectedCard = ({
+  player,
+  selectedPlayer,
+  setSelectedPlayer,
+  price,
+  setPrice,
+}) => {
+  const deleteSelectedPlayer = (dPlayer) => {
+    toast.success(`${dPlayer.name} deleted!`);
+    const newPrice = price + parseInt(dPlayer.price);
+    setPrice(newPrice);
+    setSelectedPlayer(
+      selectedPlayer.filter((sPlayer) => sPlayer.name !== dPlayer.name),
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-between border-2 border-gray-300 p-5 rounded-xl">
+      <div className="flex gap-4 items-center">
+        <div>
+          <img
+            className="h-20 w-20 rounded-xl"
+            src={player.profile_image}
+            alt={player.name}
+          />
+        </div>
+        <div>
+          <h3 className="font-bold">{player.name}</h3>
+          <p className="text-[#131313]/60">{player.player_type}</p>
+        </div>
+      </div>
+      <div
+        onClick={() => deleteSelectedPlayer(player)}
+        className="cursor-pointer"
+      >
+        <RiDeleteBin6Line size={25} className="text-red-600" />
+      </div>
+    </div>
+  );
+};
+
+export default SelectedCard;

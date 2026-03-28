4cdbd66ece8253e5e13bdef3b87506b9ebd6c5f6
nayon-kumar <nayon.cse55@gmail.com>
2026-03-29 00:36:17 +0600

    Navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,91 +22,93 @@ const Navbar = ({ price }) => {
   }, []);
 
   return (
-    <div className="relative" ref={menuRef}>
-      <div className="flex justify-baseline gap-2 sm:justify-between bg-base-100 shadow-sm px-3 py-2">
-        <div
-          className="flex items-center sm:hidden"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? <IoClose size={25} /> : <IoMenu size={25} />}
-        </div>
-        <div>
-          <img className="cursor-pointer" src={logo} alt="Logo" />
-        </div>
-        <div className="hidden sm:flex">
-          <div className="flex items-center">
-            <Link
-              to="/"
-              className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-            >
-              Home
-            </Link>
-            <Link
-              to="/"
-              className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-            >
-              Fixture
-            </Link>
-            <Link
-              to="/"
-              className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-            >
-              Teams
-            </Link>
-            <Link
-              to="/"
-              className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-            >
-              Schedules
-            </Link>
-            <button className="btn btn-outline btn-primary">
-              {price} Cr{" "}
-              <HiCurrencyDollar className="text-[#E88102]" size={25} />
-            </button>
+    <div className="fixed z-10 w-full">
+      <div className="relative" ref={menuRef}>
+        <div className="flex justify-baseline gap-2 sm:justify-between bg-base-100 shadow-sm px-3 py-2">
+          <div
+            className="flex items-center sm:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? <IoClose size={25} /> : <IoMenu size={25} />}
           </div>
-        </div>
-        {isOpen ? (
-          <>
-            <div className="absolute top-20 left-0 bg-white">
-              <div className="flex flex-col">
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  to="/"
-                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-                >
-                  Home
-                </Link>
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  to="/"
-                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-                >
-                  Fixture
-                </Link>
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  to="/"
-                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-                >
-                  Teams
-                </Link>
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  to="/"
-                  className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
-                >
-                  Schedules
-                </Link>
-                <button className="btn">
-                  {price} Cr{" "}
-                  <HiCurrencyDollar className="text-[#E88102]" size={25} />
-                </button>
-              </div>
+          <div>
+            <img className="cursor-pointer" src={logo} alt="Logo" />
+          </div>
+          <div className="hidden sm:flex">
+            <div className="flex items-center">
+              <Link
+                to="/"
+                className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+              >
+                Home
+              </Link>
+              <Link
+                to="/"
+                className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+              >
+                Fixture
+              </Link>
+              <Link
+                to="/"
+                className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+              >
+                Teams
+              </Link>
+              <Link
+                to="/"
+                className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+              >
+                Schedules
+              </Link>
+              <button className="btn btn-outline btn-primary">
+                {price} Cr{" "}
+                <HiCurrencyDollar className="text-[#E88102]" size={25} />
+              </button>
             </div>
-          </>
-        ) : (
-          <></>
-        )}
+          </div>
+          {isOpen ? (
+            <>
+              <div className="absolute top-20 left-0 bg-white">
+                <div className="flex flex-col">
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/"
+                    className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/"
+                    className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                  >
+                    Fixture
+                  </Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/"
+                    className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                  >
+                    Teams
+                  </Link>
+                  <Link
+                    onClick={() => setIsOpen(false)}
+                    to="/"
+                    className="hover:bg-primary hover:text-white px-5 py-2 rounded-md"
+                  >
+                    Schedules
+                  </Link>
+                  <button className="btn">
+                    {price} Cr{" "}
+                    <HiCurrencyDollar className="text-[#E88102]" size={25} />
+                  </button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
